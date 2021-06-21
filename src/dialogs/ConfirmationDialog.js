a9747e992b6c01d839c4be174453e14fe2ec881a
Brandon Swenson <bmswens@gmail.com>
2021-06-21 10:12:30 +0000

    <ConfirmationDialog> passes tests

--- src/dialogs/ConfirmationDialog.js
+++ src/dialogs/ConfirmationDialog.js
@@ -3,14 +3,48 @@ import React from 'react'
 
 // Material UI
 import Dialog from '@material-ui/core/Dialog'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import { DialogContent, DialogContentText, DialogActions } from '@material-ui/core'
+import Button from '@material-ui/core/Button'
 
 function ConfirmationDialog(props) {
 
+    const { open, setOpen, text, callback } = props
+
+    function close() {
+        setOpen(false)
+    }
+
+    function confirm() {
+        callback()
+        close()
+    }
+
     return (
         <Dialog
-
+            open={open}
+            onClose={close}
         >
-
+            <DialogTitle>
+                Confirmation Required
+            </DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    {text}
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button
+                    onClick={close}
+                >
+                    Cancel
+                </Button>
+                <Button
+                    onClick={confirm}
+                >
+                    Confirm
+                </Button>
+            </DialogActions>
         </Dialog>
     )
 }
--- src/dialogs/ConfirmationDialog.test.js
+++ src/dialogs/ConfirmationDialog.test.js
@@ -4,7 +4,8 @@ import React from 'react'
 // testing lib
 import {  
     render,
-    screen
+    screen,
+    fireEvent
 } from '@testing-library/react'
 
 // to test
