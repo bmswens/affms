5d70d6fa2a375526227af1780eb63a9c200123da
Brandon Swenson <bmswens@gmail.com>
2021-06-21 10:06:41 +0000

    tests added for <ConfirmationDialog>

---
+++ src/dialogs/ConfirmationDialog.js
@@ -0,0 +1,18 @@
+// React
+import React from 'react'
+
+// Material UI
+import Dialog from '@material-ui/core/Dialog'
+
+function ConfirmationDialog(props) {
+
+    return (
+        <Dialog
+
+        >
+
+        </Dialog>
+    )
+}
+
+export default ConfirmationDialog
---
+++ src/dialogs/ConfirmationDialog.test.js
@@ -0,0 +1,46 @@
+// React
+import React from 'react'
+
+// testing lib
+import {  
+    render,
+    screen
+} from '@testing-library/react'
+
+// to test
+import ConfirmationDialog from './ConfirmationDialog'
+
+describe('<ConfirmationDialog>', function() {
+    let close
+    let callback
+    beforeEach(() => {
+        close = jest.fn()
+        callback = jest.fn()
+        render(
+            <ConfirmationDialog
+                open={true}
+                setOpen={close}
+                callback={callback}
+                text={"Testing text"}
+            />
+        )
+    })
+    it('should contain a title and warning message', function() {
+        let title = screen.getByRole('heading', {name: "Confirmation Required"})
+        let text = screen.getByText(/Testing text/)
+        expect(title).not.toBeNull()
+        expect(text).not.toBeNull()
+    })
+    it('should have a functional cancel button', function() {
+        let cancelButton = screen.getByRole('button', {name: 'Cancel'})
+        fireEvent.click(cancelButton)
+        expect(close).toHaveBeenCalled()
+        expect(callback).not.toHaveBeenCalled()
+    })
+    it('should have a functional confirm button', function() {
+        let confirmButton = screen.getByRole('button', {name: 'Confirm'})
+        fireEvent.click(confirmButton)
+        expect(callback).toHaveBeenCalled()
+        expect(close).toHaveBeenCalled()
+    })
+})
