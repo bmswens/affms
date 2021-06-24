1b053854d4ed2121b7b7082b8c1986bc17ce5570
Brandon Swenson <bmswens@gmail.com>
2021-06-24 06:59:56 +0000

    <ActionBar> stylel updated

--- src/people/ActionBar.test.js
+++ src/people/ActionBar.test.js
@@ -14,22 +14,19 @@ describe('<ActionBar />', function() {
     beforeEach(() => {
         render(<ActionBar />)
     })
-    it('has a functional "Add New Person" button', async function() {
-        let dialog = screen.queryByRole('dialog')
-        expect(dialog).toBeNull()
-        let addNewButton = screen.getByRole('button', {name: 'Add New Person'})
-        fireEvent.click(addNewButton)
+    it('should have a "Delete All People" button', async function() {
+        let downloadButton = screen.getByRole('button', {name: "Delete All People"})
+        expect(downloadButton).not.toBeNull()
+        fireEvent.click(downloadButton)
         await waitFor(() => {
-            dialog = screen.queryByRole('dialog')
-            expect(dialog).not.toBeNull()
-            let title = screen.queryByText(/Add a Member/)
-            expect(title).not.toBeNull()
+            let text = screen.queryByText(/Not Implemented/)
+            expect(text).not.toBeNull()
         })
     })
-    it('has a an "Upload New Person" button', async function() {
-        let uploadNewButton = screen.getByRole('button', {name: "Upload New Person"})
-        expect(uploadNewButton).not.toBeNull()
-        fireEvent.click(uploadNewButton)
+    it('should have a "Download From Cloud" button', async function() {
+        let downloadButton = screen.getByRole('button', {name: "Download From Cloud"})
+        expect(downloadButton).not.toBeNull()
+        fireEvent.click(downloadButton)
         await waitFor(() => {
             let text = screen.queryByText(/Not Implemented/)
             expect(text).not.toBeNull()
@@ -42,6 +39,36 @@ describe('<ActionBar />', function() {
         await waitFor(() => {
             let text = screen.queryByText(/Not Implemented/)
             expect(text).not.toBeNull()
+        })
+    })
+    it('has a an "Upload New Person" button', async function() {
+        let uploadNewButton = screen.getByRole('button', {name: "Upload Person Or People"})
+        expect(uploadNewButton).not.toBeNull()
+        fireEvent.click(uploadNewButton)
+        await waitFor(() => {
+            let text = screen.queryByText(/Not Implemented/)
+            expect(text).not.toBeNull()
+        })
+    })
+    it('should have a "Upload To Cloud" button', async function() {
+        let downloadButton = screen.getByRole('button', {name: "Upload To Cloud"})
+        expect(downloadButton).not.toBeNull()
+        fireEvent.click(downloadButton)
+        await waitFor(() => {
+            let text = screen.queryByText(/Not Implemented/)
+            expect(text).not.toBeNull()
+        })
+    })
+    it('has a functional "Add New Person" button', async function() {
+        let dialog = screen.queryByRole('dialog')
+        expect(dialog).toBeNull()
+        let addNewButton = screen.getByRole('button', {name: 'Add New Person'})
+        fireEvent.click(addNewButton)
+        await waitFor(() => {
+            dialog = screen.queryByRole('dialog')
+            expect(dialog).not.toBeNull()
+            let title = screen.queryByText(/Add a Member/)
+            expect(title).not.toBeNull()
         })
     })
 })
